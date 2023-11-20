1a84de2d4987066ce2e558996fcdec6ca909314f
Exter-dg <prkandmap23@rocketmail.com>
2023-11-20 16:56:16 +0530

    Project initialization

--- bin/user_data_cdk.ts
+++ bin/user_data_cdk.ts
@@ -3,19 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { UserDataCdkStack } from '../lib/user_data_cdk-stack';
 
+
+/*
+  *******************
+
+  - This CDK app implements the following tutorial using CDK
+  https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-dynamo-db.html
+  
+  *******************
+*/
+
 const app = new cdk.App();
-new UserDataCdkStack(app, 'UserDataCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new UserDataCdkStack(app, 'UserDataCdkStack', {});
--- lib/user_data_cdk-stack.ts
+++ lib/user_data_cdk-stack.ts
@@ -6,11 +6,5 @@ export class UserDataCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'UserDataCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
